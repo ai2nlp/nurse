f2247e5fb5ec951161b5d7f3f44b8b2eb28658d9
ai2nlp <montrealsystemarchitects@gmail.com>
2026-06-14 20:40:53 -0400

    expose config.js to fix live site (anon key is public by design)

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL     = 'https://qobsznttpnrlzqjodyym.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvYnN6bnR0cG5ybHpxam9keXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzOTIzMTUsImV4cCI6MjA5Njk2ODMxNX0.IJrBlo7CL3xjbFBfSSTOov_FKflzsazewDrjp7EVIrs';
